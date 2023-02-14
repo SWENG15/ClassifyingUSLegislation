04d3db148d7d28d350f682921cf6bb0a402e1e72
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-02-14 18:02:49 +0000

    Fix lints

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+"""This module is primarily as an example for testing."""
+
 def example_function():
+    """This is an example function just here to be tested."""
     return True
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import pytest
-from main import *
+"""This is a module for testing."""
 
 def test_example_function():
+    """This function asserts that the test function is true."""
     assert True
