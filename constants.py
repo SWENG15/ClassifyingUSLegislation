8725b0709448b68394e9117a58e2142bf5553b04
Alex Todeush <todeusha@tcd.ie>
2023-02-15 17:26:03 +0000

    Now using PyLegiscan, can now search for bills and get their text

--- constants.py
+++
@@ -1,2 +0,0 @@
-example_passed = "1523556"
-example_vetoed = "74914"
--- getBill.py
+++ getBill.py
@@ -1,23 +1,49 @@
-import env
-import constants as const
-
-import requests
 import json
+import env
+from legiscan import LegiScan
+from bs4 import BeautifulSoup
+import requests
+import base64
 
-api_key = env.API_KEY
-bill_id = const.example_vetoed
 
-url = f"https://api.legiscan.com/?key={api_key}&op=getBill&id={bill_id}"
+#Given a base 64 encoded html document, return only the text
+def extract_bill_text(base64_enc):
+    doc_html = base64.b64decode(doc_text64)
+    soup = BeautifulSoup(doc_html, "html.parser")
+    content = soup.find('div', {'class':'document'})
+    print(content.text)
+    return doc_html
 
-response = requests.get(url)
-data = response.json()
+#Create Legiscan API sessions
+legis = LegiScan(env.API_KEY)
 
-filename = f"pulled_bills/bill_{bill_id}.json"
-with open(filename, 'w') as f:
-    json.dump(data, f)
+#Define search
+bills = legis.search(state='sc', query='status:passed')
+bills['summary']
 
-print(f"Data written to file: {filename}")
+for b in bills['results']:
+    bill_id = b['bill_id']
+    bill_text_url = b['text_url']
 
-#To be used for batch calls
-#url = f"https://api.legiscan.com/?key={api_key}&op=getSearch&state=FL&keyword=status%3Apassed"
-#url = f"https://api.legiscan.com/?key={api_key}&op=getSearch&state=FL&keyword=status%3Avetoed"
+    print("Bill ID: " + str(bill_id))
+
+    #Write bill jsons
+    url = f"https://api.legiscan.com/?key={env.API_KEY}&op=getBill&id={bill_id}"
+    response = requests.get(url)
+    data = response.json()
+    filename = f"pulled_bills/bill_{bill_id}.json"
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+    print(f"Data written to file: {filename}")
+
+    #Find number of texts and select most recent one
+    num_texts = len(data['bill']['texts'])
+    if(num_texts > 0):
+        bill_doc_id = data['bill']['texts'][num_texts - 1]['doc_id']
+        doc_text64 = legis.get_bill_text(bill_doc_id).get('doc')
+        document_text = extract_bill_text(doc_text64)
+        
+    print("Bill Text URL: " + str(bill_text_url))
+    print("Doc ID: " + str(bill_doc_id) + "\n")
+
+
