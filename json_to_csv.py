92c28dc21e528807b6328f6418bcd8f5c793df19
FengchenHu <fehu@tcd.ie>
2023-02-16 18:08:26 +0000

    ETL

--- json_to_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import constants as const
-
-bill_id = const.example_vetoed
-filename = f"pulled_bills/bill_{bill_id}.json"
-
-df = pd.read_json(filename)
-df.to_csv(f'pulled_bills/bill_{bill_id}.csv')
