463ab3cf187e3160c61687f70236524e989cdfb6
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-02-24 13:00:25 +0000

    Set up frontend/backend integration
    Currently the button to submit the bill does not actually do anything -can't figure out why, passing over to James

---
+++ Pyscript Website/service.py
@@ -0,0 +1,18 @@
+import model as ml
+
+# Once seperate states are added, this should be run when a state is selected, and each state's model should be remembered
+classifier, vectorizer = ml.train_model('subject_dataset.csv')
+
+# Disable undefined variable as the class is defined at runtime
+# pylint: disable=undefined-variable
+submitted_bill = Element("bill")
+
+def submit_bill():
+    """submit_bill runs when a new bill's text is submitted"""
+    print("Submitted succesfully \n")
+    submitted_bill.clear()
+    if not submitted_bill.element.value:
+        print("Bill empty")
+    else:
+        print(ml.predict_subject(classifier,vectorizer,submitted_bill.element.value))
+    
