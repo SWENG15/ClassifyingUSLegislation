4f2d5a987c9a78957bb941dac4cc17ddcef820dc
FengchenHu <fehu@tcd.ie>
2023-02-27 14:47:50 +0000

    ETL

--- ETL_pipeline/get_bill.py
+++ ETL_pipeline/get_bill.py
@@ -61,7 +61,7 @@ with open(CSV_FILENAME, 'w', encoding='UTF-8') as csvfile:
             data = response.json()
             filename = f"pulled_bills/bill_{bill_id}.json"
             with open(filename, 'w', encoding='UTF-8') as f:
-                 json.dump(data, f)
+                      json.dump(data, f)
 
             #Get bill status number and find text equivalent
             bill_status = codes.BILL_STATUS[data['bill']['status']]
@@ -72,22 +72,22 @@ with open(CSV_FILENAME, 'w', encoding='UTF-8') as csvfile:
 
             #Only write to csv if the text field won't be empty
             if num_texts > 0:
-               bill_doc_id = data['bill']['texts'][num_texts - 1]['doc_id']
-               print("Doc ID: " + str(bill_doc_id) + "\n")
-               doc_text64 = legis.get_bill_text(bill_doc_id).get('doc')
-               document_text = "\"" + extract_bill_text(doc_text64, QUERY_STATE) + "\""
-               print(document_text)
+                bill_doc_id = data['bill']['texts'][num_texts - 1]['doc_id']
+                print("Doc ID: " + str(bill_doc_id) + "\n")
+                doc_text64 = legis.get_bill_text(bill_doc_id).get('doc')
+                document_text = "\"" + extract_bill_text(doc_text64, QUERY_STATE) + "\""
+                print(document_text)
 
-               num_subjects = len(data['bill']['subjects'])
-               # pylint: disable=invalid-name
-               bill_subject = "No Subject Provided"
-               if num_subjects > 0:
-                  #Get bill subject matter
-                  bill_subject = data['bill']['subjects'][0]['subject_name']
-                  print("Bill Subject: " + str(bill_subject))
-               else:
-                  print("No Bill Subject")
+                num_subjects = len(data['bill']['subjects'])
+                # pylint: disable=invalid-name
+                bill_subject = "No Subject Provided"
+                if num_subjects > 0:
+                   #Get bill subject matter
+                   bill_subject = data['bill']['subjects'][0]['subject_name']
+                   print("Bill Subject: " + str(bill_subject))
+                else:
+                   print("No Bill Subject")
 
-               #Write all relevant bill information into csv
-               csv_row = [bill_id, bill_title, document_text, bill_status, bill_subject]
-               csvwriter.writerow(csv_row)
+                #Write all relevant bill information into csv
+                csv_row = [bill_id, bill_title, document_text, bill_status, bill_subject]
+                csvwriter.writerow(csv_row)
