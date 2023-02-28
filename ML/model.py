c43335209168f84a159a297ce0738ba34706e842
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-02-28 15:18:04 +0000

    Revert "Add backend for states"
    This reverts commit f10683c8c6091c0c6ed459d47172c1a167226eef.

--- ML/model.py
+++ ML/model.py
@@ -9,7 +9,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
-#from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score
 
 def train_model(training_data='../ETL_pipeline/dataset.csv'):
     """train_model trains a Naive Bayes classifier for subject matter based on 
