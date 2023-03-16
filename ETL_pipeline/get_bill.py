711d6e2fdd56a7186c3ddf220b0207bd1fce92c1
Alex Todeush <todeusha@tcd.ie>
2023-03-16 17:44:02 +0000

    Fixed pull test

--- ETL_pipeline/get_bill.py
+++ ETL_pipeline/get_bill.py
@@ -39,11 +39,12 @@ def get_bills_from_search(query_state, search_query, csv_name, num_pages, legi_e
     """Given a search state and query, produce a csv file of the relevant information"""
     #Make a list of already pulled bills to avoid duplicates
     already_pulled = []
-    with open('already_pulled.txt', 'r+', encoding='UTF-8') as pulled:
-        contents = pulled.read()
-        already_pulled = contents.split()
     if csv_name == "pytest.csv":
         already_pulled = []
+    else:
+        with open('already_pulled.txt', 'r+', encoding='UTF-8') as pulled:
+            contents = pulled.read()
+            already_pulled = contents.split()
     print(already_pulled)
     csv_filename = csv_name
     header = ['ID', 'Title', 'Text', 'Status', 'Subject']
