98b7f37ea6c1ee8ba9d8b29ff07ca21e8a28a4b7
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-03-29 11:23:22 +0100

    Remove repetition in testing
    The random state is now always the same, so there is no need to repeat the accuracy testing.

--- etl_pipeline/dataset_test.py
+++ etl_pipeline/dataset_test.py
@@ -7,10 +7,6 @@ import pytest
 from ..etl_pipeline.analyse_dataset import analyse_data
 from ..ML.subject_model import train_model_accuracy
 
-# This is the number of times the accuracy is calculated for each dataset
-# to find a better estimate of the accuracy
-NUM_TESTS = 20
-
 test_data = [
     ("etl_pipeline/datasets/west-virginia-dataset.csv",0.5)
 ]
@@ -25,12 +21,7 @@ def test_state_accuracy(path,expected):
     """
     This function tests the accuracy of a dataset's model
     """
-    total = 0
-    for _ in range(0,NUM_TESTS):
-        total += train_model_accuracy(path)
-
-    accuracy = total/NUM_TESTS
-    assert accuracy > expected
+    assert train_model_accuracy(path) > expected
 
 @pytest.mark.parametrize("path",state_list)
 def test_state_validity(path):
