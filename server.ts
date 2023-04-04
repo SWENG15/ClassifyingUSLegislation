259228c014db898bdd1c40c7940af6777a320bbd
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-04-04 14:15:13 +0100

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
+import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
+
+const serveFiles = (req: Request) => staticFiles('.')({
+    request: req,
+    respondWith: (r: Response) => r
+})
+
+serve((req) => serveFiles(req), { addr: ':8080' });
