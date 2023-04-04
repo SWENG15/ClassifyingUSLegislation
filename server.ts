1fcf7c807790dc8ae89eb6f4bfdb10d4ed6c3d3f
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-04-04 17:46:57 +0100

    Remove unnecessary file

--- server.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
-import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
-
-const serveFiles = (req: Request) => staticFiles('.')({
-    request: req,
-    respondWith: (r: Response) => r
-})
-
-serve((req) => serveFiles(req), { addr: ':8080' });
