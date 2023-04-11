363062d4cf8b3dedeb1c9677d935290f4e86dd33
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2023-04-11 10:08:46 +0100

    lint

--- Pyscript_Website/service.py
+++ Pyscript_Website/service.py
@@ -5,8 +5,6 @@
 import subject_model as ml
 import pass_model as pml
 import bill_similarity_model as bsm
-import threading
-import time
 
 # Pylint labels these as constants incorrectly
 # pylint: disable=invalid-name
@@ -78,35 +76,17 @@ def change_state(new_state, states_dict):
     print('probability fine\n')
     new_sim_model = bsm.load_model(new_state)
     print('similar fine\n')
-    # Case that new state is actually new
-    #if state != new_state:
-        # If the model for this state has already been trained, use that model
-      #  if new_state in states:
-       #     new_classifier = states_dict[state][0]
-        #    new_subject_vectorizer = states_dict[state][1]
-         #   new_pmodel = states_dict[state][2]
-          #  new_pvectorizer = states_dict[state][3]
-           # new_sim_model = states_dict[state][4]
-
-        # If the model for this state has not already been trained, train it
-        # and add it to the dictionary of models
-        #else:
-         #   new_classifier, new_subject_vectorizer = ml.train_model(str(new_state)+'-dataset.csv')
-          #  new_pmodel, new_pvectorizer = pml.train_regression_model(str(new_state)+'-dataset.csv')
-           # print('probability fine\n')
-            #new_sim_model = bsm.train_similarity_model(str(new_state)+'-dataset.csv')
-            #states[new_state] = (
-             #   new_classifier,new_subject_vectorizer,new_pmodel,new_pvectorizer,new_sim_model
-            #)"""
     # pylint: disable=line-too-long
     return new_state, new_classifier, new_subject_vectorizer, new_pmodel, new_pvectorizer, new_sim_model, states
 
 
-def remove_overlay(): 
+def remove_overlay():
+    """
+    This function is called when the startup is completed,
+    and it removes the loading overlay.
+    """
+    # pylint: disable=undefined-variable
     Element('load_overlay').clear()
     Element('load_overlay').remove_class("Load__screen__overlay")
-    stop_flag = True
-
 
 remove_overlay()
-
